9cf04be7a250c3547287bda80ed9ae7b12c0059d
Artur Kudla <arturkapes@gmail.com>
2019-03-14 08:01:24 +0300

    feature: add getZerosCount logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var number2 = number;
+  var base2 = base;
+  for (var i = 2; i <= base; i++) {
+    if (base2 % i === 0) {
+      var k = 0;
+      while (base2 % i === 0) {
+        base2 = Math.floor(base2 / i);
+        k++;
+    }
+    var number3 = number;
+    var m = 0;
+    while (number3 / i > 0) {
+      m = m + Math.floor(number3 / i);
+      number3 = Math.floor(number3 / i);
+    }
+    if (number2 > m / k) { 
+      number2 = m / k;
+    } 
+  }
+}
+return Math.floor(number2);
 }
